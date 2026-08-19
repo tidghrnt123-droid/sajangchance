c31efbc918decd642c0f650d4ba7d256042e9cd5
사장님찬스 <562-88@naver.com>
2026-08-19 14:33:05 +0900

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,11 +81,63 @@ export default function RootLayout({
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* Meta Pixel noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=28514603764813743&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
+
       <body className="min-h-full flex flex-col pb-[76px] md:pb-0">
         {children}
 
         <FloatingButtons />
 
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {
+              if(f.fbq)return;
+              n=f.fbq=function(){
+                n.callMethod
+                  ? n.callMethod.apply(n,arguments)
+                  : n.queue.push(arguments)
+              };
+
+              if(!f._fbq)f._fbq=n;
+
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s);
+
+            }(
+              window,
+              document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js'
+            );
+
+            fbq('init', '28514603764813743');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* AceCounter */}
         <Script id="acecounter" strategy="afterInteractive">
           {`
             var _AceGID = (function () {
