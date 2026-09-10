657cfbb5f5c531a8182a938033d937021663a61a
사장님찬스 <562-88@naver.com>
2026-09-10 12:11:23 +0900

    Remove AceCounter script

--- app/layout.tsx
+++ app/layout.tsx
@@ -124,7 +124,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        {/* Meta Pixel noscript */}
+        {/* =========================
+            Meta Pixel noscript
+        ========================= */}
         <noscript>
           <img
             height="1"
@@ -199,106 +201,6 @@ export default function RootLayout({
               'track',
               'PageView'
             );
-          `}
-        </Script>
-
-        {/* =========================
-            AceCounter
-        ========================= */}
-        <Script
-          id="acecounter"
-          strategy="afterInteractive"
-        >
-          {`
-            var _AceGID = (function () {
-
-              var Inf = [
-                'sajangchance.com',
-                'www.sajangchance.com,sajangchance.com',
-                'AZ1A105990',
-                '1',
-                'NaPm,Ncisy',
-                '1'
-              ];
-
-              var _CI =
-                (!_AceGID)
-                  ? []
-                  : _AceGID.val;
-
-              var _N = 0;
-
-              if (
-                _CI
-                  .join('.')
-                  .indexOf(
-                    Inf[2]
-                  ) < 0
-              ) {
-                _CI.push(
-                  Inf
-                );
-
-                _N =
-                  _CI.length;
-              }
-
-              return {
-                o: _N,
-                val: _CI
-              };
-
-            })();
-
-            var _AceCounter = (function () {
-
-              var G =
-                _AceGID;
-
-              var _sc =
-                document.createElement(
-                  'script'
-                );
-
-              var _sm =
-                document.getElementsByTagName(
-                  'script'
-                )[0];
-
-              if (
-                G.o !== 0
-              ) {
-                var _A =
-                  G.val[
-                    G.o - 1
-                  ];
-
-                var _U =
-                  _A[4].replace(
-                    /,/g,
-                    '_'
-                  );
-
-                _sc.src =
-                  'https://cr.acecounter.com/ac.js' +
-                  '?gc=' +
-                  _A[2] +
-                  '&py=' +
-                  _A[1] +
-                  '&up=' +
-                  _U +
-                  '&rd=' +
-                  new Date().getTime();
-
-                _sm.parentNode.insertBefore(
-                  _sc,
-                  _sm
-                );
-
-                return _sc.src;
-              }
-
-            })();
           `}
         </Script>
 
